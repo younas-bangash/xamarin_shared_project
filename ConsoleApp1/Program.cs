21cfe8436414a70220c7aab351402f3b32da9551
Muhammad Younas <engr.younasbangash@gmail.com>
2018-04-09 23:25:29 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SharedProject;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create object of the string class
+            Strings strings = new Strings();
+
+            System.Diagnostics.Debug.WriteLine(strings.newLine);
+        }
+    }
+}
---
+++ SharedProject/Strings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject
+{
+    public class Strings
+    {
+        // Declare without initializing.
+        string message1;
+
+        // Initialize to null.
+        string message2 = null;
+
+        // Initialize as an empty string.
+        // Use the Empty constant instead of the literal "".
+        string message3 = System.String.Empty;
+        
+        public string newLine = "Hello word \n Hello word";
+        
+        public string newLine2 = @"Hello word \n Hello word";
+
+        // Use System.String if you prefer.
+        System.String greeting = "Hello World!";
+
+        // Use a const string to prevent 'message4' from
+        // being used to store another string value.
+        public const string message4 = "You can't get rid of me!";
+
+    }
+}
